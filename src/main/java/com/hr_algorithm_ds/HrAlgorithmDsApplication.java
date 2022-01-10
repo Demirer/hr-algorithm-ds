a2bb20d73cfc1044072f05d921a4ee4e41c32f3e
Demirer <o.demirer@outlook.com>
2022-01-10 14:11:58 +0300

    Initial commit.

---
+++ src/main/java/com/hr_algorithm_ds/HrAlgorithmDsApplication.java
@@ -0,0 +1,12 @@
+package com.hr_algorithm_ds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrAlgorithmDsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HrAlgorithmDsApplication.class, args);
+    }
+}
